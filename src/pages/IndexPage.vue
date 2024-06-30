da72294b9b87792625a79b837f227bb58f52bcf1
Debitcardxp7 <152225363+Debitcardxp7@users.noreply.github.com>
2024-06-30 13:14:26 -0700

    Changed main Layout and added center for CSS

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,15 +1,11 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page>
+    <div class="center"><h3>Food Vendor Reviewer!</h3></div>
   </q-page>
 </template>
 
 <script setup>
 defineOptions({
-  name: 'IndexPage'
+  name: "IndexPage",
 });
 </script>
